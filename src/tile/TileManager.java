e7b178a8bec3b12fa2b1e42ae407edb569cc9ec1
nicolae.tabirca <nicolae.tabirca@stud.acs.upb.ro>
2026-02-16 15:57:03 +0200

    now drawing only the tiles the player sees

--- src/tile/TileManager.java
+++ src/tile/TileManager.java
@@ -210,8 +210,13 @@ public class TileManager {
             int screenX = worldX - gamePanel.player.worldX + gamePanel.player.screenX;
             int screenY = worldY - gamePanel.player.worldY + gamePanel.player.screenY;
 
-            graphics2D.drawImage(tile[tileNumber].image,
-                    screenX, screenY, gamePanel.tileSize, gamePanel.tileSize, null);
+            if (worldX + gamePanel.tileSize > gamePanel.player.worldX - gamePanel.player.screenX
+                    && worldX - gamePanel.tileSize < gamePanel.player.worldX + gamePanel.player.screenX
+                    && worldY + gamePanel.tileSize > gamePanel.player.worldY - gamePanel.player.screenY
+                    && worldY - gamePanel.tileSize < gamePanel.player.worldY + gamePanel.player.screenY) {
+                graphics2D.drawImage(tile[tileNumber].image, screenX, screenY,
+                        gamePanel.tileSize, gamePanel.tileSize, null);
+            }
             worldCol++;
 
             if (worldCol == gamePanel.maxWorldCol) {
